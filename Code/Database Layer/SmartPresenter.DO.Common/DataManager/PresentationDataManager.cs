ff9d54039dba36551026b4b78dc58c53d3898269
Vibhore <vibhore.tanwer@yahoo.com>
2016-03-30 13:48:20 +0530

    Missing files and code added.

---
+++ Code/Database Layer/SmartPresenter.DO.Common/DataManager/PresentationDataManager.cs
@@ -0,0 +1,35 @@
+﻿using SmartPresenter.Common.Logger;
+using SmartPresenter.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPresenter.Data.Common
+{
+    public class PresentationDataManager
+    {
+        /// <summary>
+        /// Loads the presentation.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException">Specified presentation is not found</exception>
+        public static PresentationDTO LoadPresentation(string path)
+        {
+            Logger.LogEntry();
+
+            if (File.Exists(path) == false)
+            {
+                throw new FileNotFoundException("Specified presentation is not found", path);
+            }
+            Serializer<PresentationDTO> serializer = new Serializer<PresentationDTO>();
+            PresentationDTO presentationDTO = (PresentationDTO)serializer.Load(path);
+
+            Logger.LogExit();
+            return presentationDTO;
+        }
+    }
+}
---
+++ Code/Database Layer/SmartPresenter.Data.Entities/Media/Factory/DefaultShapeFactory.cs
@@ -0,0 +1,59 @@
+﻿using SmartPresenter.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPresenter.Data.Entities
+{
+    /// <summary>
+    /// class for creating shape factories
+    /// </summary>
+    internal class DefaultShapeFactor
+    {
+        #region Method
+
+        /// <summary>
+        /// Creates the factory based on the type given.
+        /// </summary>
+        /// <param name="elementType">Type of the element.</param>
+        /// <returns></returns>
+        public ShapeDTO CreateShape(ElementType elementType)
+        {
+            switch(elementType)
+            {
+                case ElementType.Audio:
+                    return new AudioDTO();                    
+                case ElementType.Circle:
+                    return new CircleDTO();                    
+                case ElementType.DVD:
+                    return new DVDDTO();                    
+                case ElementType.Ellipse:
+                    return new EllipseDTO();                    
+                case ElementType.Image:
+                    return new ImageDTO();                    
+                case ElementType.LiveVideo:
+                    return new LiveVideoDTO();                                    
+                case ElementType.Polygon:
+                    return new PolygonDTO();                    
+                case ElementType.Rectangle:
+                    return new RectangleDTO();                    
+                case ElementType.Square:
+                    return new SquareDTO();                    
+                case ElementType.Text:
+                    return new TextDTO();                    
+                case ElementType.Triangle:
+                    return new TriangleDTO();                    
+                case ElementType.Video:
+                    return new VideoDTO();                    
+                case ElementType.Path:
+                case ElementType.Line:
+                default:
+                    return null;                    
+            }            
+        }
+
+        #endregion        
+    }
+}
